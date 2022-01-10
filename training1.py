f24635a4fc1089049ac28d0397831dca8c0ca8a2
rishabhmoolya <rishabhmoolya.rm@gmail.com>
2022-01-10 18:11:16 +0100

    updates

---
+++ training1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 10:10:30 2022
+
+@author: Rishabh
+"""
+
+def distance(a,b):
+    '''This function returns the distance between two numbers.'''
+    x = abs(b-a)
+    return x
+
+def geometric_mean(a,b):
+    '''It returns the GM of two numbers'''    
+    y = (a*b)**0.5
+    return y
+
+def pyramid_volume(A,h):
+    '''It returns the volume of a pyramid with base area A and height H.'''
+    z = (A*h)/3
+    return z
+    
