e24a013ae46bfb74ca6208418f5af8e7350b0f95
Rishabh Moolya <rishabhmoolya.rm@gmail.com>
2022-01-11 18:45:57 +0100

    update

---
+++ training3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 18:28:52 2022
+
+@author: moolyari
+"""
+
+import math
+
+
+def degree(x):
+    """ It takes the value in radians and returns the converted value in degrees."""
+    theta = x* 360/(2* math.pi)
+    return theta
+
+
+def min_max(m):
+    """ It computes the minimum of the elements and returns the maxium value of the elements in the form of a tuple"""
+    m = []
+    xs  = m. min()
+    return xs
+    
