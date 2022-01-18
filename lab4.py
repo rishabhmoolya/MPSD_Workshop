2caa21c4d8c1be281f7633ce839ae5077720ee43
rishabhmoolya <rishabhmoolya.rm@gmail.com>
2022-01-18 21:15:37 +0100

    update

--- lab4.py
+++ lab4.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jan 12 23:02:47 2022
 
-@author: Moin
+@authors: Moin, Gargi, Rishabh
 """
 
 import math
@@ -12,7 +12,7 @@ import numpy as np
 def seq_sqrt(xs):
     s1 = []
     for i in range(len(xs)):
-        if xs[i] > 0:
+        if xs[i] >= 0:
             s = math.sqrt(xs[i])
             s1 = np.append(s1, s)
         else:
@@ -21,22 +21,21 @@ def seq_sqrt(xs):
 
 
 def mean(xs):
-    s1 = []
-    s = 0
+    s = 0.0
+    assert s.mean([0]) == 0.0
     for i in range(len(xs)):
         s = s + xs[i]
         avg = s/len(xs)
-
     print(avg)
 
 
 def wc(filename):
-    with open(filename, 'r') as f:   
+    with open(filename, 'r') as f:
         lines = f.readlines()
     y1 = 0
     for elem in lines:
         y = elem.split()
         if len(y) > 0:
-           y1 = y1 + len(y) 
-           #y1.append(len(y))
+            y1 = y1 + len(y)
+            # y1.append(len(y))
     print(y1)
